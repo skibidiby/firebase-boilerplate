d0e76987cdaf83a062d21b142025b36b46088135
skibidiby <skibidiby@outlook.com>
2021-09-02 15:02:00 +0300

    ✨ stay signed on refresh

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import React from "react";
-import Routes from './utilities/Routes'
+import Routes from "./utilities/Routes";
 import { BrowserRouter as Router, Switch } from "react-router-dom";
 import "./App.css";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./firebase-config";
+import { useDispatch } from "react-redux";
+import { setActiveUser } from "./redux/user/userSlice";
 
 dotenv.config();
 function App() {
+  const dispatch = useDispatch();
+  onAuthStateChanged(auth, (result) => {
+    if (result) {
+      console.log(result);
+      dispatch(
+        setActiveUser({
+          // name: result.user.displayName,
+          email: result.email,
+        })
+      );
+    }
+  });
   return (
     <div className="App">
-        <Router>
-          <Switch>
-            <Routes />
-          </Switch>
-        </Router>
+      <Router>
+        <Switch>
+          <Routes />
+        </Switch>
+      </Router>
     </div>
   );
 }
